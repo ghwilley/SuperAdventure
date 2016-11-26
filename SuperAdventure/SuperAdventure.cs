3ed5c98ceff3f14040446ed8209e7588b33ab461
Grant <ghwilley@gmail.com>
2016-11-25 20:13:51 -0600

    added labels

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -16,5 +16,20 @@ namespace SuperAdventure
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
