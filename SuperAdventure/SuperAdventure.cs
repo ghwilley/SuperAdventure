75731cda9a343cf6f17f0e0df31f31cc0adb82ab
Grant <ghwilley@gmail.com>
2016-11-29 13:38:12 -0600

    adde constructors for location and quest

--- SuperAdventure/SuperAdventure.cs
+++ SuperAdventure/SuperAdventure.cs
@@ -21,11 +21,7 @@ using Engine;
         {
             InitializeComponent();
 
-
-            Location location = new Location();
-            location.ID = 1;
-            location.Name = "Home";
-            location.Description = "This is your house.";
+            Location location = new Location(1,"Home","This is your house.");
 
             _player = new Player();
 
